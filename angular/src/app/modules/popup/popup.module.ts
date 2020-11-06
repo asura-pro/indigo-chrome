a2316ca0aba14b3777d32f313fe05d71513a8430
xcorpio <ngxcorpio@gmail.com>
2020-11-06 21:10:10 +0800

    初始化项目

---
+++ angular/src/app/modules/popup/popup.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+
+import { PopupComponent } from './pages/popup/popup.component'
+import { PopupRoutingModule } from './popup-routing.module'
+
+@NgModule({
+  declarations: [PopupComponent],
+  imports: [CommonModule, PopupRoutingModule]
+})
+export class PopupModule { }
